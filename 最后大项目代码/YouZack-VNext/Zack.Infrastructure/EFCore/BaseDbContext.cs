71748c0ece7d1eadcef1b6423ce852891640911a
yangzhongke <yzk365@qq.com>
2022-01-27 11:32:37 +0800

    make IMediator of BaseDbContext nullable

--- 最后大项目代码/YouZack-VNext/Zack.Infrastructure/EFCore/BaseDbContext.cs
+++ 最后大项目代码/YouZack-VNext/Zack.Infrastructure/EFCore/BaseDbContext.cs
@@ -10,9 +10,9 @@ namespace Zack.Infrastructure.EFCore
 {
     public abstract class BaseDbContext : DbContext
     {
-        private IMediator mediator;
+        private IMediator? mediator;
 
-        public BaseDbContext(DbContextOptions options, IMediator mediator) : base(options)
+        public BaseDbContext(DbContextOptions options, IMediator? mediator) : base(options)
         {
             this.mediator = mediator;
         }
@@ -34,7 +34,10 @@ namespace Zack.Infrastructure.EFCore
             //在写入数据库前触发领域事件，因为领域事件的Handler也都属于领域，所以通过聚合根获取数据，所以可以读取到还没有提取的数据。
             //这样可以保证所有连接在同一个连接中。集成事件一定要在SaveChanges之后。
             //当然，其他领域对象如果直接去读数据库，是读不到这次的修改，因此要通过EF Core的FindAsync()方法读取，因为默认是读取本地缓存
-            await mediator.DispatchDomainEventsAsync(this);
+            if(mediator!=null)
+            {
+                await mediator.DispatchDomainEventsAsync(this);
+            }            
             var softDeletedEntities = this.ChangeTracker.Entries<ISoftDelete>()
                 .Where(e => e.State == EntityState.Modified && e.Entity.IsDeleted)
                 .Select(e => e.Entity).ToList();//在提交到数据库之前，记录那些被“软删除”实体对象。一定要ToList()，否则会延迟到ForEach的时候才执行
