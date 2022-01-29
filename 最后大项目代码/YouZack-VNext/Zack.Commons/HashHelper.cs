1aac35f9a42a63f178b3d5d1f431a3ac17ea256a
yangzhongke <yzk365@qq.com>
2022-01-29 19:40:37 +0800

    add more methods

--- 最后大项目代码/YouZack-VNext/Zack.Commons/HashHelper.cs
+++ 最后大项目代码/YouZack-VNext/Zack.Commons/HashHelper.cs
@@ -6,17 +6,31 @@ namespace Zack.Commons
 {
     public static class HashHelper
     {
+        private static string ToHashString(byte[] bytes)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+
         public static string ComputeSha256Hash(Stream stream)
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
                 byte[] bytes = sha256Hash.ComputeHash(stream);
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
-                {
-                    builder.Append(bytes[i].ToString("x2"));
-                }
-                return builder.ToString();
+                return ToHashString(bytes);
+            }
+        }
+
+        public static string ComputeSha256Hash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return ToHashString(bytes);
             }
         }
 
@@ -24,13 +38,17 @@ namespace Zack.Commons
         {
             using (MD5 md5Hash = MD5.Create())
             {
-                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-                StringBuilder sBuilder = new StringBuilder();
-                for (int i = 0; i < data.Length; i++)
-                {
-                    sBuilder.Append(data[i].ToString("x2"));
-                }
-                return sBuilder.ToString();
+                byte[] bytes = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return ToHashString(bytes);
+            }
+        }
+
+        public static string ComputeMd5Hash(Stream input)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] bytes = md5Hash.ComputeHash(input);
+                return ToHashString(bytes);
             }
         }
     }
