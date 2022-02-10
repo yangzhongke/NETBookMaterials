cc326a166e26714e048abd605ccc25ad0fc865e7
yangzhongke <yzk365@qq.com>
2022-02-10 16:07:18 +0800

    fix bug

--- 最后大项目代码/YouZack-VNext/Zack.DomainCommons/Models/BaseEntity.cs
+++ 最后大项目代码/YouZack-VNext/Zack.DomainCommons/Models/BaseEntity.cs
@@ -1,18 +1,14 @@
 ﻿using MediatR;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Zack.DomainCommons.Models
 {
     public record BaseEntity : IEntity, IDomainEvents
     {
 
-        /*
-        [NotMapped]//这样有点不好，通过代码配置方式来降低依赖，单一职责。不过影响不大，所以就用Attribute吧
-        public List<INotification> DomainEvents { get; }
-            = new List<INotification>();*/
-        //好像在.NET 5中，这个DomainEvents必须为public 属性+[NotMapped]，因为ChangeTracker跟踪的是一个拷贝，而不是本来这个对象
-        //但是好像到了.NET6中，DomainEvents为字段也可以了
+        [NotMapped]
         private List<INotification> domainEvents = new();
 
         public Guid Id { get; protected set; } = Guid.NewGuid();
