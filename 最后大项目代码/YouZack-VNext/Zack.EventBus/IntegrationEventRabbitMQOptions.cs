7bb5df3022fbf676668c50cc7e929e3d134f2142
netcore <76742407+bocblca@users.noreply.github.com>
2022-09-21 15:35:46 +0800

    Update IntegrationEventRabbitMQOptions.cs
    add rabbitmq connect username password

--- 最后大项目代码/YouZack-VNext/Zack.EventBus/IntegrationEventRabbitMQOptions.cs
+++ 最后大项目代码/YouZack-VNext/Zack.EventBus/IntegrationEventRabbitMQOptions.cs
@@ -4,5 +4,7 @@
     {
         public string HostName { get; set; }
         public string ExchangeName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
